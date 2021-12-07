d3b7a27c3b74a1fe5b76cb0c127d8914e75a260f
Wojciech Jarosz <jaroszw@gmail.com>
2021-12-07 13:37:24 +0100

    fs module, create, write, rename, Sync Async await, create dir ...

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
-const os = require("os");
+const fsPromises = require("fs").promises;
 const path = require("path");
-const { add, subtrack } = require("./math");
 
-// console.log(__dirname);
-// console.log(__filename);
+const fileOps = async () => {
+  try {
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "Hello my name is Wojtek"
+    );
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf-8"
+    );
 
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
 
-// console.log(path.parse(__filename));
+    console.log("DATA", data);
 
-// console.log(add(2, 3));
-// console.log(subtrack(6, 3));
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "promisedWrite.txt"),
+      `${data}`
+    );
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "promisedWrite.txt"),
+      `\n\nNice to meet you`
+    );
+
+    await fsPromises.rename(
+      path.join(__dirname, "files", "promisedWrite.txt"),
+      path.join(__dirname, "files", "helloChained.txt")
+    );
+
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "helloChained.txt"),
+      "utf-8"
+    );
+    console.log(newData);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fileOps();
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "reply.txt"),
+//   "Nice to met you",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Write complete");
+
+//     fs.appendFile(
+//       path.join(__dirname, "files", "reply.txt"),
+//       " \n\n Appending text",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("Append complete");
+
+//         fs.rename(
+//           path.join(__dirname, "files", "reply.txt"),
+//           path.join(__dirname, "files", "renamed.txt"),
+//           (err) => {
+//             if (err) throw err;
+//             console.log("Renamed complete");
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncought error: ${err}`);
+  process.exit();
+});
